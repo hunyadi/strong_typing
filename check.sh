e7ec18e9d2f93a9b4fb42584cb30b782654917c9
Levente Hunyadi <hunyadi@gmail.com>
2025-03-30 17:51:56 +0200

    Add script for GitHub Actions

--- check.sh
+++ check.sh
@@ -1,12 +1,12 @@
 set -e
 
-PYTHON=python3
+PYTHON_EXECUTABLE=${PYTHON:-python3}
 
 # Run static type checker and verify formatting guidelines
-$PYTHON -m mypy strong_typing
-$PYTHON -m flake8 strong_typing
-$PYTHON -m mypy tests
-$PYTHON -m flake8 tests
+$PYTHON_EXECUTABLE -m mypy strong_typing
+$PYTHON_EXECUTABLE -m flake8 strong_typing
+$PYTHON_EXECUTABLE -m mypy tests
+$PYTHON_EXECUTABLE -m flake8 tests
 
 # Run unit tests
-$PYTHON -m unittest discover
+$PYTHON_EXECUTABLE -m unittest discover
